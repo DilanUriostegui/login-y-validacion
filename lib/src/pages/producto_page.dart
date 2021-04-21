44a67f30ab708e370e2e85068c1ac19c7f19eaf8
Dilan <duriostegui@asgbpm.com>
2021-04-21 16:37:40 -0500

    Subir fotografias desde flutter y mostrarlas en la aplicación, carga y actualización de fotos en la aplicación y mostrarlas, fin de la sección 12

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -139,13 +139,17 @@ class _ProductoPageState extends State<ProductoPage> {
     );
   }
 
-  void _submit(){
+  void _submit() async{
 
     if( !formKey.currentState.validate() ) return;
 
     formKey.currentState.save();
 
     setState(() {_guardando = true;});
+
+    if ( foto != null ){
+      producto.fotoUrl = await productoProvider.subirImagen(foto);
+    }
 
 
     if( producto.id == null ){
@@ -171,7 +175,14 @@ class _ProductoPageState extends State<ProductoPage> {
 
   Widget _mostrarFoto() {
     if (producto.fotoUrl != null) {
-      return Container();
+      return FadeInImage(
+        placeholder: AssetImage('assets/jar-loading.gif'), 
+        image: NetworkImage( producto.fotoUrl ),
+        height: 300.0,
+        fit: BoxFit.contain,
+      );
+
+
     } else {
       if (foto != null) {
         return Image.file(
--- lib/src/providers/productos_provider.dart
+++ lib/src/providers/productos_provider.dart
@@ -1,6 +1,9 @@
 
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:mime_type/mime_type.dart';
 
 import 'package:formvalidation/src/models/producto_model.dart';
 
@@ -65,6 +68,40 @@ class ProductosProvider{
     print( json.decode(resp.body));
 
     return 1;
+  }
+
+
+  Future<String> subirImagen( File imagen ) async{
+
+    final url = Uri.parse('https://api.cloudinary.com/v1_1/dztl2m8fb/image/upload?upload_preset=tybpb7ek');
+    final mimeType = mime(imagen.path).split('/');//image/jpeg
+
+    final imageUploadRequest = http.MultipartRequest(
+      'POST',
+      url
+    );
+
+    final file = await http.MultipartFile.fromPath(
+      'file', 
+      imagen.path,
+      contentType: MediaType(mimeType[0], mimeType[1] ),
+    );
+
+    imageUploadRequest.files.add(file);
+
+    final streamResponse = await imageUploadRequest.send();
+    final resp = await http.Response.fromStream(streamResponse);
+    if( resp.statusCode != 200 && resp.statusCode != 201 ){
+      print('Algo salio mal');
+      print(resp.body);
+      return null;
+    }
+
+    final respData = json.decode(resp.body);
+
+    print(respData);
+    return respData['secure_url'];
+
   }
 
 }
